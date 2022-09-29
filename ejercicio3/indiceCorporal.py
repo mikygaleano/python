c9576ac8a2c2bd60ede295221ecc8617e0c041b9
mikygaleano <creatormikeonline@gmail.com>
2022-09-29 19:22:55 -0300

    python3

---
+++ ejercicio3/indiceCorporal.py
@@ -0,0 +1,8 @@
+peso = int(input('Ingrese su peso, por favor: '))
+talla = float(input('Ingrese su altura en metros, por favor: '))
+
+def imc ():
+    calculo = round(peso / pow(talla, 2), 2)
+    print(f'Su indice de masa corporal es: {str(calculo)}')
+
+imc()
