5e02db14acce5fe0aa07b283f81d88d66ad93b32
mikygaleano <creatormikeonline@gmail.com>
2022-10-01 17:39:13 -0300

    python

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python
-bootcamp python
+OpenBootcamp python
+
---
+++ ejercicios/ejercicio5.py
@@ -0,0 +1,8 @@
+def añoBisiesto ():
+    añoIngresado = int(input(f'ingrese un año por favor: '))
+    if (añoIngresado % 4 == 0 and añoIngresado % 100 != 0) or añoIngresado % 400 == 0:
+        print(f'el año: {añoIngresado} ingresado es bisiesto')
+    else:
+        print(f'el año: {añoIngresado} ingresado no es bisiesto')
+
+añoBisiesto()
