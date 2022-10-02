fe9f55901f648d133b97ebe07354767cfc27a123
mikygaleano <creatormikeonline@gmail.com>
2022-10-02 11:23:16 -0300

    python

---
+++ ejercicio6.py
@@ -0,0 +1,20 @@
+# clases
+
+class vehiculo:
+    color : None
+    ruedas : None
+    puertas : None
+
+class coche (vehiculo):
+    velocidad : None
+    cilindrada : None
+
+v = vehiculo()
+
+c = coche()
+
+c.color = 'naranja'
+
+print(c.color)
+
+
