2143226e7f014a6bbbb9a3b1f9ebcc008ede52bf
mikygaleano <creatormikeonline@gmail.com>
2022-10-04 19:11:44 -0300

    python

--- ejercicios/ejercicio8_modulo.py
+++ ejercicios/ejercicio8_modulo.py
@@ -1,6 +1,5 @@
 # modulo calculadora
 
-
 def suma (a, b):
     print(a + b)
 
@@ -12,3 +11,42 @@ def multiplicar (a, b):
 
 def divicion (a, b):
     print(a / b)
+
+def calculadora():
+    while True:
+        print('******** Bienvenido a su calculadora *********')
+        elegir = input('pulse "espacio" para apagar o "a" para seguir: ')
+        if elegir == ' ':
+            print('Chau!!')
+            break
+        if elegir == 'a':
+            print('Ingrese los dos numeros que desea calcular')
+            num1 = int(input('Numero 1: '))
+            num2 = int(input('Numero 2: '))
+            print('Por favor ingrese la operación que desea realizar')
+            print('******* Menu *******')
+            print('opcion "1" suma')
+            print('opcion "2" resta')
+            print('opcion "3" multiplicar')
+            print('opcion "4" dividir')
+            print('opcion "5" finalizar calculadora')
+            opcion = int(input('ingrese su opcion a continuación: '))
+
+            if opcion == 5:
+                print('Adios!!')
+                break
+            elif opcion == 1:
+                print(suma(num1, num2))
+            elif opcion == 2:
+                print(resta(num1, num2))
+            elif opcion == 3:
+                print(multiplicar(num1, num2))
+            elif opcion == 4:
+                print(divicion(num1, num2))
+            else:
+                print('Opción invalida')
+    
+        
+        
+
+    
