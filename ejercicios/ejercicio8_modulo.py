93868353713cde654876aceef4b5179f652e2088
mikygaleano <creatormikeonline@gmail.com>
2022-10-04 16:37:45 -0300

    python

---
+++ ejercicios/ejercicio8_modulo.py
@@ -0,0 +1,14 @@
+# modulo calculadora
+
+
+def suma (a, b):
+    print(a + b)
+
+def resta (a, b):
+    print(a - b)
+
+def multiplicar (a, b):
+    print(a * b)
+
+def divicion (a, b):
+    print(a / b)
---
+++ ejercicios/main.py
@@ -0,0 +1,9 @@
+import ejercicio8_modulo
+
+ejercicio8_modulo.suma(8, 5)
+
+ejercicio8_modulo.resta(8, 5)
+
+ejercicio8_modulo.multiplicar(8, 5)
+
+ejercicio8_modulo.divicion(8, 5)
