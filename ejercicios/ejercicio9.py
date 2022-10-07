e1af8aabdd0a61b3012ab8afd43bd16f8e6c704a
mikygaleano <creatormikeonline@gmail.com>
2022-10-07 16:57:51 -0300

    python

--- ejercicios/ejercicio9.py
+++ ejercicios/ejercicio9.py
@@ -1,17 +1,22 @@
+
 import time
+
+hora = time.strftime('%H:%M:%S')
+horaIngreso = time.strftime('08:00:00')
+horaSalida = time.strftime('19:00:00')
+
+def irte ():
+    print(horaIngreso - hora)
 
 while True:
     
-    horaEntrada = '08:00:00'
-    hora = time.strftime('%H:%M:%S')
-
-    if hora == '19:00:00':
+    if hora == horaSalida:
         print(f'Son las {hora} debes irte a casa!!')
-    elif hora < '19:00:00':
-        print(f'Faltan { hora - 19:00:00 } para irte a casa')
+    elif hora < horaSalida:
+        print(f'Falta para irte a casa')
         time.sleep(5)
-    elif hora > '19:00:00' or hora < '08:00:00':
-        print('Aún falta para ir al trabajo')
+    elif hora > horaSalida or hora < horaIngreso:
+        print(f'Aún falta para ir al trabajo')
         time.sleep(5)
     else:
         print('Descansa!!')
