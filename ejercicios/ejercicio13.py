4bc9da8a573e17f116488f1819cfb62ccf862f05
mikygaleano <creatormikeonline@gmail.com>
2022-10-16 18:43:17 -0300

    python

---
+++ ejercicios/ejercicio13.py
@@ -0,0 +1,5 @@
+# En este segundo ejercicio, tenéis que crear una aplicación
+# que obtendrá los elementos impares de una lista
+# pasada por parámetro con filter y
+# realizará una suma de todos estos elementos obtenidos mediante reduce.
+
